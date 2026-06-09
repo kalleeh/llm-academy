a8abf2b23d105e4e739b71ef5ca84afb1c338b39
Karl Wallbom <7670149+kalleeh@users.noreply.github.com>
2026-06-09 23:21:49 +0000

    feat: mount CourseProvider at app root

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { DifficultyProvider } from './DifficultyContext.tsx'
+import { CourseProvider } from './CourseContext.tsx'
 import { LanguageProvider } from './LanguageContext.tsx'
 import { ThemeProvider } from './ThemeContext.tsx'
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
       <LanguageProvider>
         <DifficultyProvider>
-          <App />
+          <CourseProvider>
+            <App />
+          </CourseProvider>
         </DifficultyProvider>
       </LanguageProvider>
     </ThemeProvider>
